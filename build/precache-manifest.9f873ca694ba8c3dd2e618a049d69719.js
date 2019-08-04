ce3ec03ef5d1488834c125ad518f2de150483175
PTMcEneaney <pagemceneaney@gmail.com>
2019-08-03 20:52:02 -0700

    update img

---
+++ build/precache-manifest.9f873ca694ba8c3dd2e618a049d69719.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06a5d7e2f526773c28136c70d83b3262",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "317ebebb468e70af30a5",
+    "url": "/memory-game/static/css/main.2d20c824.chunk.css"
+  },
+  {
+    "revision": "9547b7d7732488c4f6af",
+    "url": "/memory-game/static/js/2.8d38578f.chunk.js"
+  },
+  {
+    "revision": "317ebebb468e70af30a5",
+    "url": "/memory-game/static/js/main.b226439f.chunk.js"
+  },
+  {
+    "revision": "799512f57ffe7b17c145",
+    "url": "/memory-game/static/js/runtime~main.4bfb86a8.js"
+  }
+]);
